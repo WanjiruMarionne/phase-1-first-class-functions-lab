615ed128b83002ed8269c9f4a162d321b4d1909e
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-01-03 11:58:24 +0300

    Completed JS Advanced Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (driversArray) {
+    return (driversArray.slice(0,2));
+};
+
+const returnLastTwoDrivers = function (driversArray) {
+    return (driversArray.slice(-2));
+};
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function (fareMultiple) {
+    return function (fare) {
+        return fareMultiple * fare;
+    }
+}
+
+const fareDoubler = createFareMultiplier (2)
+
+const fareTripler = createFareMultiplier (3)
+
+function selectDifferentDrivers (driversArray, selectingDrivers) {
+    return selectingDrivers (driversArray);
+}
